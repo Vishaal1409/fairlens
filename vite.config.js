e452177831f5eb9f33a6703331a5f6faa3c3f320
Vishaal1409 <thundiojalex@gmail.com>
2026-04-23 19:59:56 +0530

    fix: add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/fairlens/",   // 🔥 ADD THIS LINE
+
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 1000,
   },
+
   plugins: [
     react(),
     tailwindcss(),
